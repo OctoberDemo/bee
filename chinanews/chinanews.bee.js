06b5bb812cba9e932afd44720ca8af0cd9db6cd3
guyiyang <guyiyang@outlook.com>
2015-12-28 08:30:19 +0800

    上线中国新闻网

---
+++ chinanews/chinanews.bee.js
@@ -0,0 +1,112 @@
+var ChinanewsBee = new _ChinanewsBee();
+function _ChinanewsBee() {
+
+    Bee.setType("www.chinanews.com");
+    Bee.setImgReferer("www.chinanews.com");
+
+    Bee.requestNoScript();
+    Bee.requestNoCss();
+    Bee.requestNoIframe();
+
+    Bee.onListLoaded = function(dom) {
+        var items = [];
+
+        var content_list = dom.byClass("content_list");
+        var links = content_list.byTags("a");
+        getItems(links);
+
+        Bee.finishExtractList(items);
+
+        function getItems(links) {
+            for (var i = 0; i < links.length; i++) {
+                if (links[i].innerText != "") {
+                    var item = {};
+                    item.title = links[i].innerText;
+                    item.url = links[i].href;
+                    item.key = Bee.hashCode(item.url);
+                    item.class = "news";
+                    //item.status = 5;
+                    items.push(item);
+                }
+            }
+        }
+    };
+
+    Bee.onItemLoaded = function(dom, item) {
+        var con = dom.byId("con", true);
+        if (con == null) {
+            console.log("未知页面格式：" + item.url);
+            Bee.passItem(item);
+            return;
+        }
+        item.title = con.byTag("h1").innerText;
+        var BaiduSpider = con.byId("BaiduSpider", true);
+        if (BaiduSpider) {
+            item.created_at = Bee.convertTime(con.byId("pubtime_baidu").innerText);
+            var source = con.byId("source_baidu").innerText;
+            if (source.indexOf("来源：") == 0 || source.indexOf("来源:") == 0) {
+                source = source.substring(3);
+            }
+            if (source.indexOf("中新网") < 0 && source.indexOf("中国新闻网") < 0) {
+                if (Bee.existSource(source)) {
+                    console.log("已知来源：" + source);
+                    Bee.passItem(item);
+                    return;
+                } else {
+                    console.log("未知来源：" + source);
+                }
+            } else {
+                console.log("source:" + source);
+                source = "中新网";
+            }
+            if (source == "") {
+                source = "中新网";
+            }
+            item.source = source;
+
+            item.content = [];
+            var tupian_div = con.byId("tupian_div", true);
+            if (tupian_div) {
+                item.content = item.content.concat(Bee.htmlToJson(tupian_div, [], [], imgConverter));
+            }
+            var left_ph = con.byClass("left_ph");
+            while (left_ph != null) {
+                item.content = item.content.concat(Bee.htmlToJson(left_ph, [], [], imgConverter));
+                var left_pt = con.byClass("left_pt");
+                item.content = item.content.concat(Bee.htmlToJson(left_pt, [], [], imgConverter));
+                left_ph.parentNode.removeChild(left_ph);
+                left_pt.parentNode.removeChild(left_pt);
+                left_ph = con.byClass("left_ph");
+            }
+
+            var left_zw = con.byClass("left_zw");
+            item.content = item.content.concat(Bee.htmlToJson(left_zw, [], [], imgConverter));
+
+            Bee.finishExtractItem(item);
+        } else {
+            console.log("No Baidu Spider!!!");
+        }
+
+    };
+
+    function imgConverter(node) {
+        var image = {};
+        image.src = node.src;
+        image.width = node.naturalWidth;
+        image.height = node.naturalHeight;
+        if (image.width < 100 || image.height < 100) {
+            return null;
+        }
+        return image;
+    }
+
+
+    this.addChannel = function(category, tag, url) {
+        Bee.addChannel(category, tag, url);
+    };
+
+    this.start = function() {
+        Bee.start();
+    }
+
+}
