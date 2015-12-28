4b11fb7b59726c1a9ea4a940ba71af513ee45b3b
guyiyang <guyy1@lenovo.com>
2015-12-28 16:28:59 +0800

    提交中国网财经频道蜜蜂1

---
+++ finance.china.bee.js
@@ -0,0 +1,63 @@
+/**
+ * Created by guyiyang on 2015/12/28.
+ */
+var chinaFinanceBee = new _ChinaFinanceBee();
+function _ChinaFinanceBee() {
+    Bee.setType("http://finance.china.com.cn/");
+    Bee.requestNoScript();
+    Bee.requestNoIframe();
+    Bee.setImgReferer("http://finance.china.com.cn/");
+    Bee.setRefreshTime(60 * 60 * 1000);
+
+    Bee.onListLoaded = function(dom) {
+        var items = [];
+        if (dom.byClass("news_list", true) != null) {
+            var itemDivs = dom.byClass("news_list").byTags("li");
+            for (var i = 0; i < itemDivs.length; i++) {
+                var item = {};
+                var tags = itemDivs[i].byTags("a", true);
+                if (tags != null) {
+                    var tag = tags[tags.length - 1];
+                    item.title = tag.innerText;
+                    item.url = tag.href;
+                    item.key = Bee.hashCode(item.url);
+                    items.push(item);
+                }
+            }
+        }
+        Bee.finishExtractList(items);
+    }
+
+    Bee.onItemLoaded = function(dom, item) {
+        setTimeout(function() {
+            var srcTag = dom.byId("source_baidu", true);
+            if (srcTag != null) {
+                var src = srcTag.innerText;
+                src.replace("来源：","");
+                if (src.search("中国网") < 0) {
+                    if (Bee.existSource(src)) {
+                        console.log("跳过来源：" + src);
+                        Bee.passItem(item);
+                        return;
+                    } else {
+                        console.log("未知来源：" + src);
+                    }
+                }
+                item.source = src;
+                var time = dom.byId("pubtime_baidu").innerText;
+                time = time.replace("发布时间：","");
+                item.created_at = Bee.convertTime(time);
+                item.status = 3;
+                if (dom.byId("content", true) != null) {
+                    var content = Bee.htmlToJson(dom.byId("content"));
+                    item.content = JSON.stringify(content);
+                    Bee.finishExtractItem(item);
+                } else {
+                    Bee.passItem(item);
+                }
+            } else {
+                Bee.passItem(item);
+            }
+        }, 1000)
+    }
+}
