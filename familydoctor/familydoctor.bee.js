d9a42b9c61968a56e40334059b7a547fc8f9ee15
guyiyang <guyiyang@outlook.com>
2016-01-14 09:25:39 +0800

    上线家庭医生在线

--- familydoctor/familydoctor.bee.js
+++ familydoctor/familydoctor.bee.js
@@ -10,17 +10,34 @@ function _FamilyDoctorBee() {
 
     Bee.onListLoaded = function(dom) {
         var items = [];
-        var content = dom.byClass("content");
-        var h4s = content.byTags("h4");
-        for (var i = 0; i < h4s.length; i++) {
-            var h4 = h4s[i];
-            h4.removeTag("span");
-            var link = h4.byTag("a");
+        var content = dom.byClass("content", true);
+        if (content) {
+            var h4s = content.byTags("h4");
+            for (var i = 0; i < h4s.length; i++) {
+                var h4 = h4s[i];
+                h4.removeTag("span");
+                var link = h4.byTag("a");
+                extractLink(link);
+            }
+        }
+        var art_lists = dom.byClasses("art-list", true);
+        for (var i = 0; i < art_lists.length; i++) {
+            var h2s = art_lists[i].byTags("h2");
+            for (var j = 0; j < h2s.length; j++) {
+                var h2 = h2s[j];
+                h2.removeTag("span");
+                var link = h2.byTag("a");
+                extractLink(link);
+            }
+        }
+
+        function extractLink(link) {
             var item = {};
             item.url = link.href;
             //item.status = 5;
             items.push(item);
         }
+
         Bee.finishExtractList(items);
     };
 
